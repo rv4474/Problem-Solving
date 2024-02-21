aa23bbf51e8657cd0b1c39eec28a07f162397061
Rajan verma <rv4474990@gmail.com>
2024-02-21 15:19:44 +0530

    added hashmap problem

--- HashMap/mapEntry.java
+++ HashMap/mapEntry.java
@@ -1,5 +1,23 @@
 package HashMap;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class mapEntry {
-    
+    //Leetcode 532
+    public int findPairs(int[] nums, int k) {
+        if(nums.length==1) return 0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:nums) {
+            map.put(i,map.getOrDefault(i,0)+1);
+            }
+        if(k==0 && map.size()==1) return 1;
+        int c=0,p=0;
+        for(Map.Entry<Integer,Integer> m : map.entrySet())
+        {
+           if(map.containsKey(m.getKey()+k)) c++;
+            if(m.getValue()>1) p++;
+        }
+        return k==0?p:c;
+    }
 }
