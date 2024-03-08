1efdad84616640449d38902ba9da7d6389946028
Rajan verma <rv4474990@gmail.com>
2024-03-08 07:49:33 +0530

    added maxFreq

---
+++ HashMap/maxFreq.java
@@ -0,0 +1,22 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class maxFreq {
+    public int maxFrequencyElements(int[] nums) {
+        int m=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+           map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+           m=Math.max(m,map.get(nums[i]));
+           set.add(nums[i]);
+        }
+        int k=0;
+        for(int a:set){
+            if(map.get(a)==m) k++;
+        }
+        return k*m;
+    }
+}
