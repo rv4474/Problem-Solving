039213e943d0a226730bfbb70793ccd4b744aa1b
Rajan verma <113434676+rv4474@users.noreply.github.com>
2024-09-19 12:04:59 +0530

    Time: 866 ms (66.43%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,7 +1,9 @@
 # Write your MySQL query statement below
-select x.Department, x.Employee , x.Salary from (
-select d.name as Department, e.name as Employee , e.salary as Salary,
-dense_rank() over(partition by d.name order by e.salary desc) as drnk
-from Employee as e join Department as d 
-on e.departmentId = d.id ) as x
+select x.Department, x.Employee , x.Salary from 
+     (
+     select d.name as Department, e.name as Employee , e.salary as Salary,
+      dense_rank() over(partition by d.name order by e.salary desc) as drnk
+     from Employee as e join Department as d 
+     on e.departmentId = d.id 
+     ) as x
 where x.drnk<=3
